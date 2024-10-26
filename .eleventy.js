818b0dbc857a71e0f76140cfc5ea25f19bf0e6f0
Jonas Purmann <joans@users.noreply.github.com>
2024-10-26 15:24:35 +0200

    feat: add build time image compression

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const { DateTime } = require('luxon');
 const readingTime = require('eleventy-plugin-reading-time');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 const htmlmin = require('html-minifier')
 const fs = require('fs');
 const path = require('path');
@@ -43,6 +44,26 @@ module.exports = function (eleventyConfig) {
       ? `<script src="${manifest['main.js']}"></script>`
       : '';
   });
+
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		// which file extensions to process
+		extensions: "html",
+
+		// Add any other Image utility options here:
+
+		// optional, output image formats
+		formats: ["webp", "jpeg", "auto"],
+		// formats: ["auto"],
+
+		// optional, output image widths
+		widths: [2500],
+
+		// optional, attributes assigned on <img> override these values.
+		defaultAttributes: {
+			loading: "lazy",
+			decoding: "async",
+		},
+	});
 
   eleventyConfig.addFilter('excerpt', (post) => {
     const content = post.replace(/(<([^>]+)>)/gi, '');
